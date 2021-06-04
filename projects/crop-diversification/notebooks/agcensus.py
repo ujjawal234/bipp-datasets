dd9aa828d9d5bdcc8b45225951b3d67a2fafaa9d
Shahbaz <mdshahbaznsr5@gmail.com>
2021-06-04 18:06:43 +0530

    resolving dependencies

---
+++ projects/crop-diversification/notebooks/agcensus.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+print("hello world")
+
+a = np.arange(10).reshape(5, 2)
+print(a)
+# import json
+# import os
+
+# # import geopandas as gpd
+# import zipfile
+# from glob import glob
+
+# import altair as alt
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
