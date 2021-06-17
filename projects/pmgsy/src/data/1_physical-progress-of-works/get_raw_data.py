8f405e321ca385902f55794fe4cb3169acc9d0ee
suryakandukuri <sudham.surya@gmail.com>
2021-06-17 18:22:40 +0530

    minor dev changes to pmgsy scraper

--- projects/pmgsy/src/data/1_physical-progress-of-works/get_raw_data.py
+++ projects/pmgsy/src/data/1_physical-progress-of-works/get_raw_data.py
@@ -1,27 +1,26 @@
+import hashlib
 import json
 from pathlib import Path
-import hashlib
-import pandas as pd
 
+import pandas as pd
 import scrapy
 from scrapy import FormRequest, Request
 from scrapy.crawler import CrawlerProcess
 
 
 class PmgsyScraper(scrapy.Spider):
-
     def __init__(self):
         self.name = "pmgsyscraper"
         super().__init__()
 
-        self.project_code = 'PPoW'
-        self.project_link = 'UspPropPhysicalProgressofWorksSubreport'
-        self.project_name = '1_physical-progress-of-works'
+        self.project_code = "PPoW"
+        self.project_link = "UspPropPhysicalProgressofWorksSubreport"
+        self.project_name = "1_physical-progress-of-works"
 
         self.project_dir = str(Path(__file__).resolve().parents[3])
         # print(self.project_dir)
-        self.parent_folder = self.project_dir + '/data/raw/'+self.project_name+'/'
-        self.output_dir = self.parent_folder+'output_files/'
+        self.parent_folder = self.project_dir + "/data/raw/" + self.project_name + "/"
+        self.output_dir = self.parent_folder + "output_files/"
         self.dataset = []
         self.failed_requests = []
 
@@ -75,7 +74,7 @@ class PmgsyScraper(scrapy.Spider):
         colab_dict = dict(zip(colab_list_values, colab_list_text))
         # looping through the states to get the districts,
         # we are starting from one since '0' is 'select state'
-        for state_code in state_codes[1:2]:
+        for state_code in state_codes[1:]:
             yield FormRequest(
                 url="http://omms.nic.in/NationalArea/National/DistrictDetails",
                 method="POST",
@@ -137,13 +136,13 @@ class PmgsyScraper(scrapy.Spider):
             block_code = block["Value"]
             block_text = block["Text"]
 
-            meta={
+            meta = {
                 "state_code": response.meta["state_code"],
                 "state_name": response.meta["state_name"],
                 "dist_code": response.meta["dist_code"],
                 "dist_name": response.meta["dist_name"],
-                "block_code":block_code,
-                "block_name":block_text,
+                "block_code": block_code,
+                "block_name": block_text,
                 "year_dict": response.meta["year_dict"],
                 "batch_dict": response.meta["batch_dict"],
                 "colab_dict": response.meta["colab_dict"],
@@ -151,38 +150,38 @@ class PmgsyScraper(scrapy.Spider):
 
             for year in meta["year_dict"]:
                 for batch_code in meta["batch_dict"]:
-                    batch_name=meta["batch_dict"][batch_code]
+                    batch_name = meta["batch_dict"][batch_code]
                     for colab_code in meta["colab_dict"]:
-                        colab_name=meta["colab_dict"][colab_code]
+                        colab_name = meta["colab_dict"][colab_code]
                         yield Request(
                             url="http://omms.nic.in/MvcReportViewer.aspx?_r=%2fPMGSYCitizen%2f{}&Level=4&State={}&District={}&Block={}&Year={}&Batch={}&Collaboration={}&PMGSY=1&LocationName={}&DistrictName={}&BlockName={}&LocalizationValue=en&BatchName={}&CollaborationName={}".format(
-                                    self.project_link,
-                                    meta['state_code'],
-                                    meta['dist_code'],
-                                    meta['block_code'],
-                                    year,
-                                    batch_code,
-                                    colab_code,
-                                    meta['state_name'],
-                                    meta['dist_name'],
-                                    meta['block_name'],
-                                    batch_name,
-                                    colab_name
-                                ),
+                                self.project_link,
+                                meta["state_code"],
+                                meta["dist_code"],
+                                meta["block_code"],
+                                year,
+                                batch_code,
+                                colab_code,
+                                meta["state_name"],
+                                meta["dist_name"],
+                                meta["block_name"],
+                                batch_name,
+                                colab_name,
+                            ),
                             method="POST",
                             meta={
                                 "state_code": meta["state_code"],
                                 "state_name": meta["state_name"],
                                 "dist_code": meta["dist_code"],
                                 "dist_name": meta["dist_name"],
-                                "block_code":meta["block_code"],
-                                "block_name":meta["block_name"],
-                                "year":year,
+                                "block_code": meta["block_code"],
+                                "block_name": meta["block_name"],
+                                "year": year,
                                 "year_dict": response.meta["year_dict"],
-                                "batch_code":batch_code,
-                                "batch_name":batch_name,
-                                "colab_code":colab_code,
-                                "colab_name":colab_name
+                                "batch_code": batch_code,
+                                "batch_name": batch_name,
+                                "colab_code": colab_code,
+                                "colab_name": colab_name,
                             },
                             callback=self.data_collector,
                             errback=self.err_handler,
@@ -194,23 +193,37 @@ class PmgsyScraper(scrapy.Spider):
         to the disk.
         """
         meta_data = dict(response.meta)
-        table = response.css('#ReportViewer_ctl09_ReportControl div div table tr td table ').get()
+        table = response.css(
+            "#ReportViewer_ctl09_ReportControl div div table tr td table "
+        ).get()
         table_list = pd.read_html(table)
         # print(table_list)
         road_data = table_list[7]
         # print(road_data)
 
-        meta_data['filename'] = None
+        meta_data["filename"] = None
         if road_data.shape[0] > 4:
             file_path = self.output_dir
-            file_name = hashlib.md5(json.dumps(meta_data).encode("utf8")).hexdigest()[:15]
-            road_data.to_csv(file_path+file_name+'.csv')
-            meta_data['filename'] = file_name
+            file_name = hashlib.md5(json.dumps(meta_data).encode("utf8")).hexdigest()[
+                :15
+            ]
+            road_data.to_csv(file_path + file_name + ".csv")
+            meta_data["filename"] = file_name
 
-            file_path = self.parent_folder + '/' + str(meta_data['state_name']) + "/" + str(meta_data['dist_name']) + "/" + str(meta_data['block_name']) + '/' + str(meta_data['year_dict'][meta_data['year']])
-            file_name = meta_data['batch_name']+'_'+meta_data['colab_name']+'.csv'
+            file_path = (
+                self.parent_folder
+                + "/"
+                + str(meta_data["state_name"])
+                + "/"
+                + str(meta_data["dist_name"])
+                + "/"
+                + str(meta_data["block_name"])
+                + "/"
+                + str(meta_data["year_dict"][meta_data["year"]])
+            )
+            file_name = meta_data["batch_name"] + "_" + meta_data["colab_name"] + ".csv"
             self.ensure_directory(file_path)
-            road_data.to_csv(file_path+'/'+file_name)
+            road_data.to_csv(file_path + "/" + file_name)
 
         self.dataset.append(meta_data)
 
@@ -220,9 +233,9 @@ class PmgsyScraper(scrapy.Spider):
         exists, if not creates the path
         """
 
-        path_parts = file_path.split('/')
-        for i in range(1, len(path_parts)+1):
-            present_path = '/'.join(path_parts[:i])
+        path_parts = file_path.split("/")
+        for i in range(1, len(path_parts) + 1):
+            present_path = "/".join(path_parts[:i])
             Path(present_path).mkdir(exist_ok=True)
 
     def err_handler(self, response):
@@ -238,12 +251,15 @@ class PmgsyScraper(scrapy.Spider):
         This function saves the dataset collecting filenames to a file on the disk
         """
 
-        print('Saving all collected data[len:{}]...'.format(len(self.dataset)))
-        json.dump(self.dataset, open(self.parent_folder+'scraped_dataset.json','w'))
+        print("Saving all collected data[len:{}]...".format(len(self.dataset)))
+        json.dump(self.dataset, open(self.parent_folder + "scraped_dataset.json", "w"))
 
         if self.failed_requests:
-            print('Saving all errs[len:{}]'.format(len(self.failed_requests)))
-            json.dumps(self.failed_requests, open(self.parent_folder+'failed_requests.json','w'))
+            print("Saving all errs[len:{}]".format(len(self.failed_requests)))
+            json.dumps(
+                self.failed_requests,
+                open(self.parent_folder + "failed_requests.json", "w"),
+            )
 
 
 def main():
