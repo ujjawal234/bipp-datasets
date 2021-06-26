bf4a6062f84d29f0463eeb3f4d0efe63fc3dd07c
Kriti Maloo <kritimaloo03@gmail.com>
2021-06-26 13:11:03 +0530

    Final Scraper for ps data for Odhisha

--- projects/psdata/src/data/3_Orrisa/psdataOrrisa.py
+++ projects/psdata/src/data/3_Orrisa/psdataOrrisa.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import pandas as pd
 import scrapy
 from scrapy import FormRequest, Request
 from scrapy.crawler import CrawlerProcess
@@ -9,8 +12,8 @@ class psdataOrrisascraper(scrapy.Spider):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
     }
 
-    # project_dir = str(Path(__file__).resolve().parents[2])
-    # parent_folder = project_dir + "/data/raw/"
+    project_dir = str(Path(__file__).resolve().parents[3])
+    parent_folder = project_dir + "/data/raw/"
 
     def start_requests(self):
         # request to initiate the scraping
@@ -19,10 +22,10 @@ class psdataOrrisascraper(scrapy.Spider):
     def parse(self, response):
         # "This fuction will parse the names of all the districts in the state and will raise another request to get all ACS in the district."
 
-        dist_names = response.xpath(
-            '//select[@id="ddlDistrict"]/option/text()'
-        ).extract()
-        print(dist_names)
+        # dist_names = response.xpath(
+        # '//select[@id="ddlDistrict"]/option/text()'
+        # ).extract()
+        # print(dist_names)
 
         dist_values = response.xpath(
             '//select[@id="ddlDistrict"]/option/@value'
@@ -65,8 +68,8 @@ class psdataOrrisascraper(scrapy.Spider):
 
         ac_list = response.xpath('//select[@id="ddlAC"]/option/text()').extract()
         ac_values = response.xpath('//select[@id="ddlAC"]/option/@value').extract()
-        print(ac_list)
-        print("*****")
+        # print(ac_list)
+        # print("*****")
         form_dict = {
             "__EVENTTARGET": response.css(
                 "#__EVENTTARGET::attr(value)"
@@ -102,7 +105,22 @@ class psdataOrrisascraper(scrapy.Spider):
 
     def save_data(self, response):
         final_table = response.xpath('//select[@id="ddlPart"]/option/text()').extract()
-        print(final_table)
+        # print(final_table)
+        table_list = pd.DataFrame(final_table, columns=["Polling_Station_Name"])
+        table_list = table_list.iloc[1:, 0:]
+
+        file_path = (
+            self.parent_folder + "/" + "3_Orrisa" + "/" + response.meta["ac_names"]
+        )
+        file_name = response.meta["ac_names"] + ".csv"
+        self.directory(file_path)
+        table_list.to_csv(file_path + "/" + file_name, index=False)
+
+    def directory(self, file_path):
+        path_parts = file_path.split("/")
+        for i in range(1, len(path_parts) + 1):
+            present_path = "/".join(path_parts[:i])
+            Path(present_path).mkdir(exist_ok=True)
 
 
 def main():
