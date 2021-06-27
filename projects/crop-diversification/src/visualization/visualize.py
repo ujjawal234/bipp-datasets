018853e425cfd42f90d963300d5b0e40fb0a0b42
Shahbaz <mdshahbaznsr5@gmail.com>
2021-06-27 18:38:52 +0530

    reduced code complexity by making functions

--- projects/crop-diversification/src/visualization/visualize.py
+++ projects/crop-diversification/src/visualization/visualize.py
@@ -7,9 +7,9 @@ import pandas as pd
 import streamlit as st
 
 path_ag = "./data/interim/agcensus_isb/"
-path_six = "ag_census_2015_2016/six_states_15_16.csv"
+path_six = "ag_census_2015_2016/mapped_nc_15_16.csv"
 rel_path = os.path.abspath(path_ag + path_six)
-non_crop_six = pd.read_csv(rel_path, index_col=0)
+
 states_six = [
     "punjab",
     "maharashtra",
@@ -19,9 +19,19 @@ states_six = [
     "karnataka",
 ]
 
+df_nc15 = pd.read_csv(rel_path, index_col=0)  # df viz. mapped for six states
+df_vis_six = df_nc15[df_nc15["state"].isin(states_six)]  # df for visualisation
+
+
+def read_topojson(path):
+    gdf = gpd.read_file(path, driver="TopoJSON")
+    return gdf
+
+
 path_tehsil_shp = "Tehsil_2020_v2_topo.json"
-rel_path_shp = os.path.abspath(path_ag + path_tehsil_shp)
-json_file = gpd.read_file(rel_path_shp, driver="TopoJSON")
+rel_path_tehsil_shp = os.path.abspath(path_ag + path_tehsil_shp)
+
+json_file = read_topojson(rel_path_tehsil_shp)
 json_file.rename(
     columns={
         "Sb_Dt_LGD": "lgd_code",
@@ -31,34 +41,42 @@ json_file.rename(
     },
     inplace=True,
 )
-json_file["id"] = json_file["lgd_code"]
-json_file.columns = json_file.columns.str.lower()
-json_file["state"] = json_file["state"].str.lower()
-json_file = json_file.applymap(lambda s: s.lower() if type(s) == str else s)
-json_file_six = json_file[json_file["state"].isin(states_six)]
-df_vis = non_crop_six[non_crop_six["lgd_code"].notna()]
-df_vis_six = df_vis[df_vis["state"].isin(states_six)]
+
+
+def pre_process(gdf, id):
+    gdf["id"] = gdf[id]
+    gdf.columns = gdf.columns.str.lower()
+    gdf["state"] = gdf["state"].str.lower()
+    gdf = gdf.applymap(lambda s: s.lower() if type(s) == str else s)
+    return gdf
+
+
+json_file = pre_process(json_file, "lgd_code")
+
+json_file_six = json_file[
+    json_file["state"].isin(states_six)
+]  # json file for six states
 
 path_state_shp = "State_2020.json"
-json_state = gpd.read_file(os.path.abspath(path_ag + path_state_shp), driver="TopoJSON")
+rel_path_state_shp = os.path.abspath(path_ag + path_state_shp)
+
+json_state = read_topojson(rel_path_state_shp)
 json_state.rename(columns={"State_LGD": "state_lgd", "stname": "state"}, inplace=True)
-json_state["id"] = json_state["state_lgd"]
-json_state["state"] = json_state["state"].str.lower()
-json_state_gdf = json_state.to_json()  # string datatype
-json_features_state = json.loads(json_state_gdf)  # dict type
-data_geo_state = alt.Data(values=json_features_state["features"])
+
+json_state = pre_process(json_state, "state_lgd")
+
+
+def json_feature_list(gdf):
+    gdf = gdf.to_json()  # string datatype
+    gdf = json.loads(gdf)  # dict type
+    gdf = alt.Data(values=gdf["features"])  # json_features['features] is list type
+    return gdf
 
 
 def state_mp(state, color_col, color_title, tooltip):
-    gdf_merged = json_file.merge(
-        df_vis_six, left_on="id", right_on="lgd_code", how="inner"
-    )  # geodataframe
-    json_gdf = gdf_merged.to_json()  # string datatype
-    json_features = json.loads(json_gdf)  # dict type
-    # json_features['features] is list type
-    data_geo = alt.Data(values=json_features["features"])
-
     # Add states layer
+    data_geo_state = json_feature_list(json_state)
+
     state_shape = (
         alt.Chart(data_geo_state)
         .mark_geoshape(stroke="white", fillOpacity=0, strokeWidth=2)
@@ -66,19 +84,21 @@ def state_mp(state, color_col, color_title, tooltip):
         .properties(width=800, height=800)
     )
     # Add Base Layer
+    gdf_merged = json_file.merge(
+        df_vis_six, left_on="id", right_on="lgd_code", how="inner"
+    )  # geodataframe
+    data_geo_tehsil = json_feature_list(gdf_merged)
+
     base = (
-        alt.Chart(data_geo, title="Visualisation for six states")
+        alt.Chart(data_geo_tehsil, title="Visualisation for six states")
         .mark_geoshape(stroke="white", strokeWidth=1)
         .encode()
         .properties(width=800, height=800)
     )
     # Add Choropleth Layer
     choro = (
-        alt.Chart(data_geo)
-        .mark_geoshape(
-            # fill='lightgray',
-            stroke="white"
-        )
+        alt.Chart(data_geo_tehsil)
+        .mark_geoshape(stroke="white")
         .encode(
             alt.Color(
                 color_col,
