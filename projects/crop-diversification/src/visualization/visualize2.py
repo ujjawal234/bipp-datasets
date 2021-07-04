9a4eb42a7498731c210b05865f31280466653e3d
Shahbaz <mdshahbaznsr5@gmail.com>
2021-07-04 22:26:09 +0530

    making multiselect year dropdown for comparisons of time-series data

--- projects/crop-diversification/src/visualization/visualize2.py
+++ projects/crop-diversification/src/visualization/visualize2.py
@@ -8,6 +8,7 @@ path_ag = "./data/interim/agcensus_isb/"
 path_nc_var = "ag_census_2015_2016/non_crop_variables_selected.xlsx"
 path_nc_ungr = "ag_census_2015_2016/combined6_mapped_nc_ungr_15_16.csv"
 path_c_ungr = "ag_census_2015_2016/combined6_mapped_c_ungr_15_16.csv"
+path_nc_ungr10 = "ag_Census_2010_2011/combined6_mapped_nc_ungr_10_11.csv"
 
 states_six = [
     "punjab",
@@ -21,6 +22,7 @@ states_drop = ["All six states"] + states_six
 
 df_nc_ungr_nc15 = pd.read_csv(os.path.abspath(path_ag + path_nc_ungr))
 df_c_ungr_nc15 = pd.read_csv(os.path.abspath(path_ag + path_c_ungr))
+df_ungr_nc10 = pd.read_csv(os.path.abspath(path_ag + path_nc_ungr10))
 
 tehsil_url = "https://raw.githubusercontent.com/Shahbaz67/bipp_personal/main/Tehsil_2020_v2_topo.json"
 data_geo_t = alt.topo_feature(tehsil_url, "Tehsil_2020_v2_new")
@@ -99,150 +101,172 @@ param_dict_rev_crop = dict(zip(param_dict_crop.values(), param_dict_crop.keys())
 colors_crop = ["yelloworangered", "goldred", "goldorange", "goldgreen"]
 color_dict_crop = dict(zip(parameter_crop, colors_crop))
 
-select_state, select_param = st.beta_columns(2)
+select_year, select_state, select_param = st.beta_columns([4, 3, 5])
 # Filter options
+select_year = select_year.multiselect(
+    "Select year", ["2010-11", "2015-16"], default=["2010-11", "2015-16"]
+)
 select_state = select_state.selectbox("Select a State", states_drop)
 # select_state.selectbox("Select a State", states_drop)
-select_crop = st.sidebar.selectbox("Crop Type", ["Crop", "Non-Crop"])
+select_crop = st.sidebar.selectbox("Crop Type", ["Non-Crop", "Crop"])
 select_soc_grp = st.sidebar.radio("Pick a social group", soc_gr)
 select_size_class = st.sidebar.radio("Pick a size class", size_class)
+
+
+@st.cache
+def load_data(data, state):
+    return data[data["state"].isin([state])]
+
+
+@st.cache
+def filter_data(data):
+    return data[
+        (data["soc_grp"] == select_soc_grp) & (data["size_class"] == select_size_class)
+    ]
+
+
+# map generate function
+@st.cache(allow_output_mutation=True)
+def generate_map(df, width, height):
+    if select_state == "All six states":
+        # df_vis = load_data(df, states_six)
+        df_vis = filter_data(df)
+        # state
+        state_layer = (
+            alt.Chart(data_geo_s)
+            .mark_geoshape(fillOpacity=0, stroke="white")
+            .properties(width=width, height=height)
+        )
+        alt.data_transformers.disable_max_rows()
+
+        # tehsil
+        tehsil_layer = (
+            alt.Chart(data_geo_t)
+            .mark_geoshape(stroke="black", strokeWidth=1)
+            .encode(
+                color=alt.Color(
+                    value,
+                    legend=alt.Legend(
+                        type="symbol",
+                        tickCount=10,
+                        symbolType="square",
+                        labelOverlap=False,
+                        symbolSize=500,
+                        columnPadding=0,
+                        rowPadding=0,
+                        symbolStrokeWidth=0,
+                    ),
+                    type="quantitative",
+                    scale=alt.Scale(scheme=color_scheme),
+                    title=field[select_param],
+                ),
+                tooltip=["state:O", "tehsil:O", value],
+            )
+            .transform_lookup(
+                lookup="properties.Sb_Dt_LGD",
+                from_=alt.LookupData(
+                    data=df_vis,
+                    key="lgd_code",
+                    fields=["state", "tehsil", field[select_param]],
+                ),
+            )
+            .properties(width=width, height=height)
+        )
+        alt.data_transformers.disable_max_rows()
+
+        # tooltip
+        tooltip_layer = (
+            alt.Chart(data_geo_t)
+            .mark_geoshape(
+                fillOpacity=0,
+            )
+            .encode(
+                color=value,
+                tooltip=["state:O", "tehsil:O", value],
+            )
+            .transform_lookup(
+                lookup="properties.Sb_Dt_LGD",
+                from_=alt.LookupData(
+                    data=df_vis,
+                    key="lgd_code",
+                    fields=["state", "tehsil", field[select_param]],
+                ),
+            )
+        )
+        alt.data_transformers.disable_max_rows()
+        map = tehsil_layer + state_layer + tooltip_layer
+    else:
+        df_vis = load_data(df, select_state)
+        df_vis = filter_data(df_vis)
+        map = (
+            alt.Chart(data_geo_t)
+            .mark_geoshape(stroke="black", strokeWidth=1)
+            .encode(
+                color=alt.Color(
+                    value,
+                    legend=alt.Legend(
+                        type="symbol",
+                        tickCount=10,
+                        symbolType="square",
+                        labelOverlap=False,
+                        symbolSize=500,
+                        columnPadding=0,
+                        rowPadding=0,
+                        symbolStrokeWidth=0,
+                    ),
+                    type="quantitative",
+                    scale=alt.Scale(scheme=color_scheme),
+                    title=field[select_param],
+                ),
+                tooltip=["state:O", "tehsil:O", value],
+            )
+            .transform_lookup(
+                lookup="properties.Sb_Dt_LGD",
+                from_=alt.LookupData(
+                    data=df_vis,
+                    key="lgd_code",
+                    fields=["state", "tehsil", field[select_param]],
+                ),
+            )
+            .properties(width=width, height=height)
+        )
+        alt.data_transformers.disable_max_rows()
+    return map
+
 
 # Dashboard setup
 if select_crop == "Non-Crop":
     select_param = select_param.selectbox(
         "select a parameter for non-crop", description
     )
-    df = df_nc_ungr_nc15
     field = param_dict_rev
     value = str(field[select_param]) + ":Q"
     color_scheme = color_dict[field[select_param]]
+    if select_year == ["2010-11"]:
+        map = generate_map(df_ungr_nc10, 700, 700)
+    elif select_year == ["2015-16"]:
+        map = generate_map(df_nc_ungr_nc15, 700, 700)
+    else:
+        map1, map2 = st.beta_columns(2)
+        map1 = generate_map(df_ungr_nc10, 450, 700)
+        map2 = generate_map(df_nc_ungr_nc15, 450, 700)
+        map = map1 | map2
 else:
     select_param = select_param.selectbox(
         "select a parameter for crop", description_crop
     )
-    df = df_c_ungr_nc15
     field = param_dict_rev_crop
     value = str(field[select_param]) + ":Q"
     color_scheme = color_dict_crop[field[select_param]]
-
-# select_state, select_param = st.beta_columns([1,1])
-
-
-@st.cache
-def load_data(data, state):
-    return data[data["state"] == state]
-
-
-@st.cache
-def filter_data(data, soc, size):
-    return data[(data["soc_grp"] == soc) & (data["size_class"] == size)]
-
-
-if select_state == "All six states":
-    df_vis_six = filter_data(df, select_soc_grp, select_size_class)
-    # state
-    state_layer = (
-        alt.Chart(data_geo_s)
-        .mark_geoshape(fillOpacity=0, stroke="white")
-        .properties(width=800, height=800)
-    )
-    alt.data_transformers.disable_max_rows()
-
-    # tehsil
-    tehsil_layer = (
-        alt.Chart(data_geo_t)
-        .mark_geoshape(stroke="black", strokeWidth=1)
-        .encode(
-            color=alt.Color(
-                value,
-                legend=alt.Legend(
-                    type="symbol",
-                    tickCount=10,
-                    symbolType="square",
-                    labelOverlap=False,
-                    symbolSize=500,
-                    columnPadding=0,
-                    rowPadding=0,
-                    symbolStrokeWidth=0,
-                ),
-                type="quantitative",
-                scale=alt.Scale(scheme=color_scheme),
-                title=field[select_param],
-            ),
-            tooltip=["state:O", "tehsil:O", value],
+    if select_year == ["2010-11"]:
+        st.text(
+            "No data available for crop (2010-11). Deselect year 2010 or change the crop type to Non-Crop from the left pane."
         )
-        .transform_lookup(
-            lookup="properties.Sb_Dt_LGD",
-            from_=alt.LookupData(
-                data=df_vis_six,
-                key="lgd_code",
-                fields=["state", "tehsil", field[select_param]],
-            ),
+    elif select_year == ["2015-16"]:
+        map = generate_map(df_c_ungr_nc15, 700, 700)
+    else:
+        st.text(
+            "No data available for crop (2010-11). Deselect year 2010 or change the crop to Non-Crop type from the left pane."
         )
-        .properties(width=800, height=800)
-    )
-    alt.data_transformers.disable_max_rows()
-
-    # tooltip
-    tooltip_layer = (
-        alt.Chart(data_geo_t)
-        .mark_geoshape(
-            fillOpacity=0,
-        )
-        .encode(
-            color=value,
-            tooltip=["state:O", "tehsil:O", value],
-        )
-        .transform_lookup(
-            lookup="properties.Sb_Dt_LGD",
-            from_=alt.LookupData(
-                data=df_vis_six,
-                key="lgd_code",
-                fields=["state", "tehsil", field[select_param]],
-            ),
-        )
-    )
-    alt.data_transformers.disable_max_rows()
-    map = tehsil_layer + state_layer + tooltip_layer
-else:
-    df_vis_six = load_data(df, select_state)
-    df_vis_six = filter_data(df_vis_six, select_soc_grp, select_size_class)
-    map = (
-        alt.Chart(data_geo_t)
-        .mark_geoshape(stroke="black", strokeWidth=1)
-        .encode(
-            color=alt.Color(
-                value,
-                legend=alt.Legend(
-                    type="symbol",
-                    tickCount=10,
-                    symbolType="square",
-                    labelOverlap=False,
-                    symbolSize=500,
-                    columnPadding=0,
-                    rowPadding=0,
-                    symbolStrokeWidth=0,
-                ),
-                type="quantitative",
-                scale=alt.Scale(scheme=color_scheme),
-                title=field[select_param],
-            ),
-            tooltip=["state:O", "tehsil:O", value],
-        )
-        .transform_lookup(
-            lookup="properties.Sb_Dt_LGD",
-            from_=alt.LookupData(
-                data=df_vis_six,
-                key="lgd_code",
-                fields=["state", "tehsil", field[select_param]],
-            )
-            # ).transform_filter(
-            #     alt.FieldEqualPredicate(
-            #         field="properties.State_UT_N", equal=select.upper()
-            #     )
-        )
-        .properties(width=800, height=800)
-    )
-    alt.data_transformers.disable_max_rows()
 
 st.altair_chart(map, use_container_width=True)
