ee32b9e0dad0d4a2ba520161f1ff1f180fc711f6
suryakandukuri <sudham.surya@gmail.com>
2021-10-04 13:43:02 +0530

    check for os and choose chromedriver by os

--- projects/wris-india/src/data/1_rainfall_data/get_raw_data.py
+++ projects/wris-india/src/data/1_rainfall_data/get_raw_data.py
@@ -3,5 +3,18 @@
 # import bs4
 # import pandas as pd
 # import selenium
+from pathlib import Path
+from sys import exit, platform
 
 # %%
+# Select the chrome driver based on the OS
+# The chrome driver will also depend on the version of the chrome running on the os
+driver_dir = str(Path(__file__).resolve().parents[0]) + "/chromedrivers/"
+if platform == "linux" or platform == "linux2":
+    driver_path = driver_dir + "linux/chromedriver"
+elif platform == "win32":
+    driver_path = driver_dir + "windows/chromedriver.exe"
+elif platform == "darwin":
+    driver_path = driver_dir + "mac/chromedriver"
+else:
+    exit("OS not supported. Exiting the program")
