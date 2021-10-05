336cd357d7e896907b818dbedde4bc9065900414
suryakandukuri <sudham.surya@gmail.com>
2021-10-05 11:53:09 +0530

    script tried with selenium

--- projects/wris-india/src/data/1_rainfall_data/get_raw_data.py
+++ projects/wris-india/src/data/1_rainfall_data/get_raw_data.py
@@ -3,18 +3,43 @@
 # import bs4
 # import pandas as pd
 # import selenium
-from pathlib import Path
-from sys import exit, platform
+import time
 
-# %%
-# Select the chrome driver based on the OS
-# The chrome driver will also depend on the version of the chrome running on the os
-driver_dir = str(Path(__file__).resolve().parents[0]) + "/chromedrivers/"
-if platform == "linux" or platform == "linux2":
-    driver_path = driver_dir + "linux/chromedriver"
-elif platform == "win32":
-    driver_path = driver_dir + "windows/chromedriver.exe"
-elif platform == "darwin":
-    driver_path = driver_dir + "mac/chromedriver"
-else:
-    exit("OS not supported. Exiting the program")
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
+# def click_dataset(id):
+#     a = driver.find_element_by_id(id)
+
+
+def selenium_scraper(dataset_name, website_url):
+    """
+    This definition is the main function that is used for scraping wris data.
+    """
+    # Initiating the link and initializing the webdriver to go to the first webpage
+    driver.execute_cdp_cmd(
+        "Network.setUserAgentOverride",
+        {"userAgent": "python 3.9", "platform": "Any"},
+    )
+    ## Calling the get_page_data function to obtain data from the web-page
+    driver.get(website_url)
+    time.sleep(10)
+    dataset = driver.find_element_by_xpath(
+        '//*[@id="downloadreporthead"]/div/div[2]/div/select'
+    )
+    dataset.click()
+
+    pass
+
+
+def main():
+    dataset_name = "Rainfall"
+    website_url = "https://indiawris.gov.in/wris/#/DataDownload"
+    selenium_scraper(dataset_name, website_url)
+
+
+if __name__ == "__main__":
+    main()
