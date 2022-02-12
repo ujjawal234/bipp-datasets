004e46d711c4ec9b73ef309169e5bd64c45d4d3c
Ritik-Arora-576 <ritikarora656@gmail.com>
2022-02-12 14:59:00 +0530

    add merge.py

--- projects/fertilizer-mis-1/src/data/1_Pos_Data/merge_data.py
+++ projects/fertilizer-mis-1/src/data/1_Pos_Data/merge_data.py
@@ -1,3 +1,26 @@
+# import liberaries
 import pandas as pd
 import os
-import numpy as np
+
+# dataframe of final output file
+dataframe= []
+
+# recursively iterate over all the csv files stored in a row folder
+def iterate(path):
+    if os.path.isfile(path):
+        if path.split('.')[-1]=='csv':
+            df = pd.read_csv(path)
+            global dataframe
+            if len(dataframe) == 0:
+                dataframe = df
+            else:
+                dataframe = pd.concat([dataframe, df], ignore_index=True)
+        return
+    for file in os.listdir(path):
+        iterate(os.path.join(path,file))
+
+# saving the dataframe
+iterate('data/raw')
+
+# save the consoladated csv file into interim folder
+dataframe.to_csv("data/interim/output.csv", index=False)
