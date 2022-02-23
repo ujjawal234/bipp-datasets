5b08b742c484d0dde4dd9e8d526eb9bd33016e61
Ritik-Arora-576 <ritikarora656@gmail.com>
2022-02-23 19:17:15 +0530

    update merge_data.py

--- projects/fertilizer-mis-1/src/data/1_Pos_Data/merge_data.py
+++ projects/fertilizer-mis-1/src/data/1_Pos_Data/merge_data.py
@@ -1,26 +1,47 @@
-# import liberaries
+# import libraries
 import pandas as pd
 import os
 
-# dataframe of final output file
-dataframe= []
+raw_path = 'data/raw'
+years = ['2015','2016','2017','2018','2019','2020','2021']
 
-# recursively iterate over all the csv files stored in a row folder
-def iterate(path):
-    if os.path.isfile(path):
-        if path.split('.')[-1]=='csv':
-            df = pd.read_csv(path)
-            global dataframe
-            if len(dataframe) == 0:
-                dataframe = df
-            else:
-                dataframe = pd.concat([dataframe, df], ignore_index=True)
-        return
-    for file in os.listdir(path):
-        iterate(os.path.join(path,file))
+for state in os.listdir(raw_path):
+    if state == 'Andaman and Nicobar Islands':
+        continue
+    if len(state.split('.')) == 1:
+        state_path = os.path.join(raw_path,state)
+        for year in years:
+            temp_path = 'data/interim'
+            temp_path = os.path.join(temp_path, state)
+            temp_path = os.path.join(temp_path, year+'.csv')
+            if os.path.isfile(temp_path):
+                print(temp_path)
+                # df = pd.read_csv(temp_path)
+                # columns = df.columns[:-3]
+                # df = df[columns]
+                # df.to_csv(temp_path, index=False, encoding='utf-8')
+                continue
+            dataframe = []
+            for dis in os.listdir(state_path):
+                dis_path = os.path.join(state_path,dis)
+                dis_path = os.path.join(dis_path,year)
+                if os.path.isdir(dis_path):
+                    for month in os.listdir(dis_path):
+                        month_path = os.path.join(dis_path, month)
+                        for day in os.listdir(month_path):
+                            day_path = os.path.join(month_path,day)
+                            if len(dataframe)==0:
+                                dataframe = pd.read_csv(day_path)
+                                columns = dataframe.columns[:-3]
+                                dataframe = dataframe[columns]
+                            else:
+                                df = pd.read_csv(day_path)
+                                columns = df.columns[:-3]
+                                df = df[columns]
+                                dataframe = pd.concat([dataframe, df], ignore_index=True)
+                            print(day_path)
+            if not os.path.isdir(os.path.join('data/interim',state)):
+                os.mkdir(os.path.join('data/interim',state))
+            dataframe.to_csv(temp_path, index=False, encoding='utf-8')
 
-# saving the dataframe
-iterate('data/raw')
-
-# save the consoladated csv file into interim folder
-dataframe.to_csv("data/interim/output.csv", index=False)
+# consolidate the data of Andaman and Nicobar Islands seperately    
