e92c973226b542047c1a1ce14fd683b00b3b432f
Ritik-Arora-576 <ritikarora656@gmail.com>
2022-03-01 14:11:53 +0530

    commit changes

--- projects/fertilizer-mis-1/src/data/1_Pos_Data/merge_data.py
+++ projects/fertilizer-mis-1/src/data/1_Pos_Data/merge_data.py
@@ -5,6 +5,28 @@ import os
 raw_path = 'data/raw'
 years = ['2015','2016','2017','2018','2019','2020','2021']
 
+# consolidate the data of Andaman and Nicobar Islands seperately
+if not os.path.isdir('data/interim/Andaman and Nicobar Islands'):
+    os.mkdir('data/interim/Andaman and Nicobar Islands')
+
+if not os.path.isfile('data/interim/Andaman and Nicobar Islands/Andaman and Nicobar Islands.csv'):
+    dataframe = []
+    for folder in os.listdir('data/raw/Andaman and Nicobar Islands/North And Middle Andaman/2015'):
+        folder_path = os.path.join('data/raw/Andaman and Nicobar Islands/North And Middle Andaman/2015',folder)
+        for file in os.listdir(folder_path):
+            file_path = os.path.join(folder_path,file)
+            print(file_path)
+            if len(dataframe)==0:
+                dataframe = pd.read_csv(file_path)
+                columns = dataframe.columns[:-3]
+                dataframe = dataframe[columns]
+            else:
+                df = pd.read_csv(file_path)
+                columns = df.columns[:-3]
+                df = df[columns]
+                dataframe = pd.concat([dataframe, df], ignore_index=True)
+    dataframe.to_csv('data/interim/Andaman and Nicobar Islands/Andaman and Nicobar Islands.csv', index=False, encoding='utf-8')
+
 for state in os.listdir(raw_path):
     if state == 'Andaman and Nicobar Islands':
         continue
@@ -16,10 +38,6 @@ for state in os.listdir(raw_path):
             temp_path = os.path.join(temp_path, year+'.csv')
             if os.path.isfile(temp_path):
                 print(temp_path)
-                # df = pd.read_csv(temp_path)
-                # columns = df.columns[:-3]
-                # df = df[columns]
-                # df.to_csv(temp_path, index=False, encoding='utf-8')
                 continue
             dataframe = []
             for dis in os.listdir(state_path):
@@ -43,5 +61,3 @@ for state in os.listdir(raw_path):
             if not os.path.isdir(os.path.join('data/interim',state)):
                 os.mkdir(os.path.join('data/interim',state))
             dataframe.to_csv(temp_path, index=False, encoding='utf-8')
-
-# consolidate the data of Andaman and Nicobar Islands seperately    
