2a4289be109ab0499ffcece5abf3ecdb36ebe48a
Dabbu100 <diyasarkar2001@gmail.com>
2022-06-15 10:07:31 +0530

    Json Flattener Script  to convert all list of dictionaries at district level into a single flattened list

--- projects/nrlm/agrinutri_scraper_name.py
+++ projects/nrlm/agrinutri_scraper_name.py
@@ -19,27 +19,21 @@ prefs = {
 chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.add_argument("start-maximized")
 
-driver = webdriver.Chrome(
-    ChromeDriverManager().install(), options=chrome_options
-)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 url = "https://nrlm.gov.in/AgriNutiGardenAction.do?methodName=showView"
 
 master_list = (
     []
 )  # Final list of all dictionaries containing names throughout all levels of data
 total_count = [0, 0, 0, 0, 0]  # Counter used to iterate through data
-district_list = (
-    []
-)  # list containing list of district-block-gp-village information
+district_list = []  # list containing list of district-block-gp-village information
 #  Scraping Process Begins
 while True:
     try:
         driver.get(url)
         driver.implicitly_wait(5)
 
-        states = driver.find_elements(
-            By.XPATH, "//table[@id='example']/tbody/tr/td[2]"
-        )
+        states = driver.find_elements(By.XPATH, "//table[@id='example']/tbody/tr/td[2]")
         # Adding state names to list
         state_name = []
         for i in range(0, len(states)):
@@ -72,11 +66,9 @@ while True:
                 total_count[2] = 0
                 total_count[3] = 0
                 total_count[4] = 0
-            if (total_count[1] < number_of_dist) and (
-                not district_json_path.exists()
-            ):
+            if (total_count[1] < number_of_dist) and (not district_json_path.exists()):
                 district_buttons[total_count[1]].click()
-                driver.implicitly_wait(3)
+                driver.implicitly_wait(10)
 
                 block_buttons = driver.find_elements(
                     By.XPATH, '//*[@id="btnwrap"]/button'
@@ -87,7 +79,7 @@ while True:
                 # Block to Gram Panchayat
                 if total_count[2] < number_of_blocks:
                     block_buttons[total_count[2]].click()
-                    driver.implicitly_wait(3)
+                    driver.implicitly_wait(10)
                     gp_buttons = driver.find_elements(
                         By.XPATH, '//*[@id="btnwrap"]/button'
                     )
@@ -97,7 +89,7 @@ while True:
                     # Gram Panchayat to Village
                     if total_count[3] < number_of_gp:
                         gp_buttons[total_count[3]].click()
-                        driver.implicitly_wait(3)
+                        driver.implicitly_wait(10)
                         village_list = driver.find_elements(
                             By.XPATH, '//*[@id="example"]/tbody[1]/tr/td[2]'
                         )
@@ -122,9 +114,7 @@ while True:
                                 total_count[4] += 1
                         else:
                             val = {}
-                            val[
-                                "state_name"
-                            ] = curr_state  # add the placeholder value for empty string values later
+                            val["state_name"] = curr_state
                             val["district_name"] = curr_dist
                             val["block_name"] = curr_block
                             val["gram_panchayat_name"] = curr_gp
@@ -140,7 +130,7 @@ while True:
                             By.XPATH, '//*[@id="backButtonId"]/input'
                         )
                         back.click()
-                        driver.implicitly_wait(3)
+                        driver.implicitly_wait(10)
                         total_count[3] += 1
 
                     if total_count[3] >= number_of_gp:
@@ -152,9 +142,7 @@ while True:
                     total_count[1] += 1
                     total_count[2] = 0
                     with open(str(district_json_path), "w") as nested_out_file:
-                        json.dump(
-                            district_list, nested_out_file, ensure_ascii=False
-                        )
+                        json.dump(district_list, nested_out_file, ensure_ascii=False)
                     district_list = []
             if total_count[1] >= number_of_dist:
                 total_count[0] += 1
@@ -162,15 +150,10 @@ while True:
             state_buttons = driver.find_elements(
                 By.XPATH, "//button[@class='idEditButton']"
             )
-            driver.implicitly_wait(3)
+            driver.implicitly_wait(10)
     except WebDriverException:
         # print(ex)
         continue
 
     if total_count[0] >= number_of_states:
         break
-jsonString = json.dumps(master_list)
-file_path = Path.joinpath(interim_path, "names_list.json")
-jsonFile = open(file_path, "w")
-jsonFile.write(jsonString)
-jsonFile.close()
