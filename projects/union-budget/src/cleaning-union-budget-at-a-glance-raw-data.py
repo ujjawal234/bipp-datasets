9daedd528606dce3fbf05963157f40c959c0b2a8
UJJAWAL JAIN <jainujjawal234@gmail.com>
2023-11-20 17:41:47 +0530

    updating file

--- projects/union-budget/src/cleaning-union-budget-at-a-glance-raw-data.py
+++ projects/union-budget/src/cleaning-union-budget-at-a-glance-raw-data.py
@@ -29,26 +29,35 @@ def remove_utf8(text):
         return ''.join(char for char in text if char.isascii())
     else:
         return text  # Return non-string values as is
+
+
 def remove_newlines(text):
     if isinstance(text, str):
         # Replace "\n" with an empty string
         return text.replace("\n", "")
     else:
         return text  # Return non-string values as is
+
+
 def remove_brackets(text):
     if isinstance(text, str):
         # Use regular expression to remove content within both parentheses and square brackets
         return re.sub(r'\([^)]*\)|\[[^\]]*\]', '', text)
     else:
         return text  # Return non-string values as is
+
+
 def data_cleaning_pipeline(df: pd.DataFrame):
     return df.pipe(extract_rows_with_data_points).map(remove_newlines).map(remove_brackets).map(remove_utf8).pipe(assign_column_names).drop(columns="hindi_head")
+
 
 def split_variable_column(df: pd.DataFrame, column_name: str = "variable"):
     df['year'] = df[column_name].str.extract(r'(\d{4}-\d{4})')
     df['estimate_type'] = df[column_name].str.replace(r'\d{4}-\d{4}', '', regex=True).str.strip()
 
     return df.drop(columns=column_name)
+
+
 cln_dfs = map(lambda df: df.pipe(data_cleaning_pipeline).melt(id_vars="english_head").pipe(split_variable_column), xls[1:])
 print(cln_dfs)
 df = pd.concat(cln_dfs)
